70cbcf83bde94bcd25e5d480cd7256fb0df45d06
Jader Moura <jader.dev.moura@gmail.com>
2025-09-30 09:04:22 -0300

    add footer e finalizando o projeto

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Header } from './components/Header/Header'
 import { Hero } from './components/Hero/Hero'
 import { Projects } from './components/Projects/Projects'
 import { AboutMe } from './components/About/AboutMe'
+import { Footer } from './components/Footer/Footer'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Hero/>
       <Projects/>
       <AboutMe/>
+      <Footer/>
     </>
   )
 }
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,45 @@
+import styles from './Footer.module.css';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import logoTexto from '../../assets/logo-texto.png';
+
+const linkedinUrl = "https://www.linkedin.com/in/dev-jader/";
+const githubUrl = "https://github.com/Jader-Moura-Lattarulo"
+
+export function Footer() {
+  return (
+    <footer className={styles.footer} id="contact">
+      <div className={styles.mainContent}>
+        
+        {/* Bloco de Contato (Esquerda) */}
+        <div className={styles.contactInfo}>
+          <div className={styles.contactBlock}>
+            <p>Meu contato:</p>
+            <strong>+55 11 99177-9858</strong>
+          </div>
+          <div className={styles.contactBlock}>
+            <p>Email:</p>
+            <strong>jader.dev.moura@gmail.com</strong>
+          </div>
+        </div>
+        
+        {/* Bloco Social (Direita) */}
+        <div className={styles.socialGroup}>
+          <img src={logoTexto} alt="Logo JMDEV" className={styles.footerLogo} />
+          <div className={styles.socialIcons}>
+            <a href={linkedinUrl} target="_blank" aria-label="GitHub">
+              <FaGithub />
+            </a>
+            <a href={githubUrl} target="_blank" aria-label="LinkedIn">
+              <FaLinkedin />
+            </a>
+          </div>
+        </div>
+
+      </div>
+      
+      <div className={styles.copyright}>
+        <p>© 2025 Jader Moura Lattarulo. Todos os direitos reservados.</p>
+      </div>
+    </footer>
+  );
+}
